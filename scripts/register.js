d3b5b90a5309e449a223295f56f9eaec158165ca
kevingonz19 <kevingonz@utexas.edu>
2022-04-05 18:38:21 -0500

    added registration requirement popup

--- scripts/register.js
+++ scripts/register.js
@@ -56,3 +56,36 @@ function register() {
 	// all criteria followed
 	window.alert("User validated")
 }
+
+var helpers = ["Your name must be in the form: first name, middle initial., last name",
+               "Your email address must have the form: user@domain",
+               "Your user ID must have at least six characters",
+               "Your password must have at least six characters and it must include one digit",
+               "This box provides advice on filling out the form on this page. Put the mouse cursor over any input field to get advice"]
+
+//ptrUser = document.getElementById("reg_user")
+function popwin(id){
+	//left = (evt.clientX - 130);
+	//top = (evt.clientY - 75);
+
+	document.getElementById(id).style.display ="block"
+	//myWin = window.open("","nCt", "left=0,width=272,height=54,status=no,toolbar=no,menubar=no,scrollbars=no");
+	//myWin.document.write("<head><title>Example</title></head>");
+	
+}
+function closewin(id){
+	document.getElementById(id).style.display ="none"
+	//if (!myWin.closed)
+	//myWin.self.close()
+}
+
+ptrPass = document.getElementById("reg_pass")
+
+// function that popups a window for the user when they hover over registration fields
+
+
+
+
+function passwordPopup(evt){
+
+}
